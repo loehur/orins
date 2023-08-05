bd7c0a57e91aef0b56b6d4ff01454149e12fc434
loehur <loehur@gmail.com>
2023-08-05 16:25:56 +0700

    export payment bleom

--- app/Views/Export/content.php
+++ app/Views/Export/content.php
@@ -1,7 +1,7 @@
 <main>
     <div class="ms-3 mt-3 me-3 bg-white">
         <div class="row">
-            <div class="col">
+            <div class="col mb-2" style="min-width:270px;max-width:350px">
                 <form action="<?php $this->BASE_URL ?>Export/export" method="POST">
                     <div class="input-group">
                         <span class="input-group-text text-primary">Penjualan</span>
@@ -11,13 +11,13 @@
                 </form>
             </div>
         </div>
-        <div class="row mt-1">
-            <div class="col">
-                <form action="<?php $this->BASE_URL ?>Export/export" method="POST">
+        <div class="row">
+            <div class="col mb-2" style="min-width:270px;max-width:350px">
+                <form action="#" method="POST">
                     <div class="input-group">
                         <span class="input-group-text text-success">Payment</span>
                         <input name="month" type="month" min="2023-07" class="form-control" required>
-                        <button type="submit" class="btn btn-primary">Export</button>
+                        <button type="submit" class="btn btn-primary" disabled>(Sabar Belom ya)</button>
                     </div>
                 </form>
             </div>
