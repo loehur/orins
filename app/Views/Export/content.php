9864de70f499de5c6d49da4388e4260e6dce6b4e
loehur <loehur@gmail.com>
2023-08-08 09:56:52 +0700

    export payment ok2

--- app/Views/Export/content.php
+++ app/Views/Export/content.php
@@ -4,7 +4,7 @@
             <div class="col mb-2" style="min-width:270px;max-width:350px">
                 <form action="<?php $this->BASE_URL ?>Export/export" method="POST">
                     <div class="input-group">
-                        <span class="input-group-text text-primary">Penjualan</span>
+                        <span class="input-group-text text-primary">Sales</span>
                         <input name="month" type="month" min="2023-07" placeholder="YYYY-MM" class="form-control" required>
                         <button type="submit" class="btn btn-primary">Export</button>
                     </div>
