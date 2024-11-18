2e6d9252f4a0e2200c42755ecaeb84aaec8b2dd3
Luhur Gunawan <loehur@gmail.com>
2024-11-18 16:13:36 +0700

    deposit32

--- app/Core/DB.php
+++ app/Core/DB.php
@@ -11,10 +11,11 @@ class DB extends DBC
     {
         $this->db_name = DBC::dbm[$db]['db'];
         $this->db_user = DBC::dbm[$db]['user'];
-        $this->db_pass = DBC::dbm[$db]['pass'];
 
         if ($this->db_pass <> "") {
-            $this->db_pass =  $_SESSION['secure']['db_pass'];
+            $this->db_pass = $_SESSION['secure']['db_pass'];
+        } else {
+            $this->db_pass = DBC::dbm[$db]['pass'];
         }
 
         $this->mysqli = new mysqli(DBC::db_host, $this->db_user, $this->db_pass, $this->db_name) or die('DB Error');
