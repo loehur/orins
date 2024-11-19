c4c6ea02533eea746f6f311c45949f46a81fcca7
Luhur Gunawan <loehur@gmail.com>
2024-11-19 14:35:34 +0700

    vitapictura222

--- app/Views/Layouts/viewer.php
+++ app/Views/Layouts/viewer.php
@@ -14,9 +14,13 @@
         if (new_parse_2 != "") {
             parse_2 = new_parse_2
         } else {
-            parse_2 = '<?= $data['parse_2'] ?>';
+            parse_2 = '<?= isset($data['parse_2']) ? $data['parse_2'] : '' ?>';
         }
         page = "<?= isset($data['page']) ? $data['page'] : 'content' ?>";
-        $("div#content").load('<?= $this->BASE_URL ?><?= $data["controller"] ?>/' + page + '/' + parse + '/' + parse_2);
+        if (parse_2 != "") {
+            $("div#content").load('<?= $this->BASE_URL ?><?= $data["controller"] ?>/' + page + '/' + parse + '/' + parse_2);
+        } else {
+            $("div#content").load('<?= $this->BASE_URL ?><?= $data["controller"] ?>/' + page + '/' + parse);
+        }
     }
 </script>
