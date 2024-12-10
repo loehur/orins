ed9135f61fc313e30e521ee35708d4d398d2c7f0
Luhur Gunawan <loehur@gmail.com>
2024-12-10 13:29:03 +0700

    cs jfaslkjfc

--- assets_main/js/autocomplete.js
+++ assets_main/js/autocomplete.js
@@ -72,7 +72,7 @@ function autocomplete(inp, arr) {
 
           b.innerHTML = textInject.trim();
 
-          console.log(txtValue.length + " " + filter.length);
+          console.log(allMatch);
 
           if (allMatch == true && txtValue.length == filter.length) {
             inp.setAttribute("data-value", this.getAttribute("data-value"));
