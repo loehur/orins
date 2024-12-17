7669c59febaf6f4d83ccf609bdeff608cef29bed
Luhur Gunawan <loehur@gmail.com>
2024-12-17 16:48:39 +0700

    rapi2sssss2ssss

--- app/Data/Barang.php
+++ app/Data/Barang.php
@@ -93,8 +93,8 @@ class Barang extends Controller
     function stok_data_proses($kode, $id_toko)
     {
         $cols = "kode_barang, CONCAT('U',sn,sds) as unic, sn, sds, sum(qty) as qty";
-        $where_masuk = "kode_barang = '" . $kode . "' AND id_target = '" . $id_toko . "' AND stat = 1 GROUP BY sn, sds";
-        $where_keluar = "kode_barang = '" . $kode . "' AND id_sumber = '" . $id_toko . "' AND stat = 1 GROUP BY sn, sds";
+        $where_masuk = "kode_barang = '" . $kode . "' AND id_target = '" . $id_toko . "' AND stat = 1 GROUP BY kode_barang, sn, sds";
+        $where_keluar = "kode_barang = '" . $kode . "' AND id_sumber = '" . $id_toko . "' AND stat = 1 GROUP BY kode_barang, sn, sds";
 
         $masuk = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_masuk, 1, "unic");
         $keluar = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_keluar, 1, "unic");
