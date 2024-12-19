2d4ef975f70c85cd47777268571b88f6a047cd93
Luhur Gunawan <loehur@gmail.com>
2024-12-19 10:57:10 +0700

    mantap nissssss

--- app/Views/Data_Operasi/content.php
+++ app/Views/Data_Operasi/content.php
@@ -118,8 +118,10 @@
                     }
                 }
 
+                $list_xdiskon = [];
                 foreach ($data['diskon'] as $ds) {
                     if ($ds['ref_transaksi'] == $ref) {
+                        $list_xdiskon[$ref] = false;
                         if ($ds['cancel'] == 0) {
                             $xtraDiskon += $ds['jumlah'];
                             $dibayar += $ds['jumlah'];
@@ -128,6 +130,7 @@
                                 if (isset($data['order'][$ref]) && is_array($data['order'][$ref])) {
                                     foreach ($data['order'][$ref] as $do) {
                                         if ($do['ref'] == $ref) {
+                                            $list_xdiskon[$ref] = true;
                                             if ($do['tuntas'] == 0) {
                                                 $showMutasi .= "<i class='fa-regular fa-circle-xmark cancel_diskon' data-id='" . $ds['id_diskon'] . "' data-bs-toggle='modal' style='cursor:pointer' data-bs-target='#modalCancelDiskon'></i> <span class='text-success'><small>XtraDiskon#" . $ds['id_diskon'] . "</small> -Rp" . number_format($ds['jumlah']) . "<br></span>";
                                                 break;
@@ -140,6 +143,10 @@
                                 }
                                 if (isset($data['mutasi'][$ref]) && is_array($data['mutasi'][$ref])) {
                                     foreach ($data['mutasi'][$ref] as $do) {
+                                        if ($list_xdiskon[$ref] == true) {
+                                            continue;
+                                        }
+
                                         if ($do['ref'] == $ref) {
                                             if ($do['tuntas'] == 0) {
                                                 $showMutasi .= "<i class='fa-regular fa-circle-xmark cancel_diskon' data-id='" . $ds['id_diskon'] . "' data-bs-toggle='modal' style='cursor:pointer' data-bs-target='#modalCancelDiskon'></i> <span class='text-success'><small>XtraDiskon#" . $ds['id_diskon'] . "</small> -Rp" . number_format($ds['jumlah']) . "<br></span>";
