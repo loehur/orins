75a036f7ac9033033f16952edb70e1747a267d54
Luhur Gunawan <loehur@gmail.com>
2024-12-23 14:31:58 +0700

    oke jaes2sss22s2wsdS

--- app/Controllers/Barang_Riwayat_B.php
+++ app/Controllers/Barang_Riwayat_B.php
@@ -42,7 +42,7 @@ class Barang_Riwayat_B extends Controller
       if ($mode == 0) {
          $data['mutasi'] = $this->db(0)->get_where('master_mutasi', "stat = 1 AND insertTime LIKE '" . $val . "%' AND (id_sumber = " . $this->userData['id_toko'] . " OR id_target = " . $this->userData['id_toko'] . ") AND jenis <> 0");
       } else {
-         $data['mutasi'] = $this->db(0)->get_where('master_mutasi', "stat = 1 AND kode_barang = '" . $val . "' ORDER BY id DESC");
+         $data['mutasi'] = $this->db(0)->get_where('master_mutasi', "stat = 1 AND kode_barang = '" . $val . "' AND (id_sumber = " . $this->userData['id_toko'] . " OR id_target = " . $this->userData['id_toko'] . ") AND jenis <> 0) ORDER BY id DESC");
       }
       $this->view(__CLASS__ . '/data', $data);
    }
