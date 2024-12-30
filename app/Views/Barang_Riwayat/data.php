319d7f69c5c71d60cb172e7721dd9d1ca86c8d0a
Luhur Gunawan <loehur@gmail.com>
2024-12-30 16:30:44 +0700

    edit oks22w22ws22ssss

--- app/Views/Barang_Riwayat/data.php
+++ app/Views/Barang_Riwayat/data.php
@@ -3,6 +3,7 @@
             $dp = $data['barang'] ?>
             <tr>
                 <td class=""><?= date('d/m/y H:i', strtotime($d['insertTime'])) ?></td>
+                <td><?= $d['ref'] ?></td>
                 <td><?= $d['sn'] ?></td>
                 <td class=""><?= $d['id_target'] == 0 ? "<span class='fw-bold text-success'>Masuk</span>" : "<span class='fw-bold text-danger'>Keluar</span>" ?></td>
                 <td class=""><?= $d['id_sumber'] == 0 ? $d['id_target'] : $d['id_sumber'] ?></td>
