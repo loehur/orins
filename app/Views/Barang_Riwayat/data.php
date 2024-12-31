68bb6c8ea3e642f73f901c6c74dc640cf68545e9
Luhur Gunawan <loehur@gmail.com>
2024-12-31 16:53:34 +0700

    dsfa2w

--- app/Views/Barang_Riwayat/data.php
+++ app/Views/Barang_Riwayat/data.php
@@ -1,9 +1,20 @@
     <table class="table table-sm">
         <?php foreach ($data['mutasi'] as $d) {
-            $dp = $data['barang'] ?>
+            $dp = $data['barang'];
+            $target_link = "";
+
+            switch ($d['tipe']) {
+                case 0:
+                    $target_link = "Gudang_Input";
+                    break;
+                case 1:
+                    $target_link = "Stok_Transfer";
+                    break;
+            }
+        ?>
             <tr>
                 <td class=""><?= date('d/m/y H:i', strtotime($d['insertTime'])) ?></td>
-                <td><a href="<?= PV::BASE_URL ?>Gudang_Input/list/<?= $d['ref'] ?>"><?= $d['ref'] ?></a></td>
+                <td><a href="<?= PV::BASE_URL . $target_link ?>/list/<?= $d['ref'] ?>"><?= $d['ref'] ?></a></td>
                 <td><?= $d['sn'] ?></td>
                 <td class=""><?= $d['id_target'] == 0 ? "<span class='fw-bold text-success'>Masuk</span>" : "<span class='fw-bold text-danger'>Keluar</span>" ?></td>
                 <td class=""><?= $d['id_sumber'] == 0 ? $d['id_target'] : $d['id_sumber'] ?></td>
