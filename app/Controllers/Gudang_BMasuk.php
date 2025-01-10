3bbdb6fb3a0d38f4730603214e5b7095ec579805
Luhur Gunawan <loehur@gmail.com>
2025-01-10 09:31:38 +0700

    asdfjh3sssssss6sssWssss

--- app/Controllers/Gudang_BMasuk.php
+++ app/Controllers/Gudang_BMasuk.php
@@ -6,7 +6,7 @@ class Gudang_BMasuk extends Controller
    {
       $this->session_cek();
       $this->data_order();
-      if (!in_array($this->userData['user_tipe'], PV::PRIV[6])) {
+      if (!in_array($this->userData['user_tipe'], PV::PRIV[7])) {
          $this->model('Log')->write($this->userData['user'] . " Force Logout. Hacker!");
          $this->logout();
       }
--- app/Controllers/Retur_Barang_C.php
+++ app/Controllers/Retur_Barang_C.php
@@ -6,7 +6,7 @@ class Retur_Barang_C extends Controller
    {
       $this->session_cek();
       $this->data_order();
-      if (!in_array($this->userData['user_tipe'], PV::PRIV[7])) {
+      if (!in_array($this->userData['user_tipe'], PV::PRIV[2])) {
          $this->model('Log')->write($this->userData['user'] . " Force Logout. Hacker!");
          $this->logout();
       }
