2122f85df29db86ee0029cf40fadbd98852c0d9b
Luhur Gunawan <loehur@gmail.com>
2025-01-11 12:02:22 +0700

    audit retursswsssd

--- app/Controllers/Cron.php
+++ app/Controllers/Cron.php
@@ -195,7 +195,7 @@ class Cron extends Controller
             exit();
          }
 
-         if ($verify_payment >= $bill && $ambil_all == true && $verify_kas_kecil == true) {
+         if ($bill > 0 && $verify_payment >= $bill && $ambil_all == true && $verify_kas_kecil == true) {
             $this->clearTuntas($ref);
          }
       } else {
