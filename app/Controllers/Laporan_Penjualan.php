4face50fdabcc3bc198c5e6a21cec9725255f333
Luhur Gunawan <loehur@gmail.com>
2025-01-15 16:34:49 +0700

    oek dfass

--- app/Controllers/Laporan_Penjualan.php
+++ app/Controllers/Laporan_Penjualan.php
@@ -65,13 +65,17 @@ class Laporan_Penjualan extends Controller
 
       $from_t = strtotime($from);
       $to_t = strtotime($to);
+
+      $from = date("Y-m-d", $from_t);
+      $to = date("Y-m-d", $to_t);
+
       $datediff = $to_t - $from_t;
       $jumlahHari = round($datediff / (60 * 60 * 24));
 
       if ($jumlahHari < 32) {
          $data['barang'] = $this->db(0)->get('master_barang', 'id');
          $cols = "id_produk, SUM(jumlah) as qty, SUM(jumlah*harga) as jumlah";
-         $where = "(id_toko = " . $this->userData['id_toko'] . " OR id_afiliasi = " . $this->userData['id_toko'] . ") AND id_pelanggan <> 0 AND (insertTime BETWEEN '" . $from . "' AND '" . $to . "') GROUP BY id_produk ORDER BY id_order_data DESC";
+         $where = "cancel = 0 AND (id_toko = " . $this->userData['id_toko'] . " OR id_afiliasi = " . $this->userData['id_toko'] . ") AND id_pelanggan <> 0 AND (SUBSTR(insertTime, 1, 10) BETWEEN '" . $from . "' AND '" . $to . "') GROUP BY id_produk ORDER BY id_order_data DESC";
          $data['order'] = $this->db(0)->get_cols_where('order_data', $cols, $where);
       }
 
@@ -88,12 +92,16 @@ class Laporan_Penjualan extends Controller
 
       $from_t = strtotime($from);
       $to_t = strtotime($to);
+
+      $from = date("Y-m-d", $from_t);
+      $to = date("Y-m-d", $to_t);
+
       $datediff = $to_t - $from_t;
       $jumlahHari = round($datediff / (60 * 60 * 24));
 
       if ($jumlahHari < 32) {
          $cols2 = "id_barang, SUM(qty) as qty, SUM(qty*harga_jual) as jumlah";
-         $where2 = "(id_sumber = " . $this->userData['id_toko'] . ") AND id_target <> 0 AND jenis = 2 AND (insertTime BETWEEN '" . $from . "' AND '" . $to . "') GROUP BY id_barang ORDER BY id DESC";
+         $where2 = "(id_sumber = " . $this->userData['id_toko'] . ") AND id_target <> 0 AND jenis = 2 AND stat = 1 AND (SUBSTR(insertTime, 1, 10) BETWEEN '" . $from . "' AND '" . $to . "') GROUP BY id_barang ORDER BY id DESC";
          $data['mutasi'] = $this->db(0)->get_cols_where('master_mutasi', $cols2, $where2);
       }
 
