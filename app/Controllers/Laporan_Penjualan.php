198a2e89e5dbab1c86490b087116c61612b9a1b0
Luhur Gunawan <loehur@gmail.com>
2025-01-15 16:13:12 +0700

    oek dfas

--- app/Controllers/Laporan_Penjualan.php
+++ app/Controllers/Laporan_Penjualan.php
@@ -6,7 +6,7 @@ class Laporan_Penjualan extends Controller
    {
       $this->session_cek();
       $this->data_order();
-      if (!in_array($this->userData['user_tipe'], PV::PRIV[8])) {
+      if (!in_array($this->userData['user_tipe'], PV::PRIV[1])) {
          $this->model('Log')->write($this->userData['user'] . " Force Logout. Hacker!");
          $this->logout();
       }
