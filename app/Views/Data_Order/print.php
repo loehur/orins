d81b061fc9dd5df6ddffcca28823370c80b3ec33
Luhur Gunawan <loehur@gmail.com>
2025-01-20 09:54:08 +0700

    oke dehssws2s3s2222s

--- app/Views/Data_Order/print.php
+++ app/Views/Data_Order/print.php
@@ -141,9 +141,6 @@
 
         if (count($data['paket']) > 0) {
             foreach ($data['paket'] as $pref => $do) {
-                echo "<pre>";
-                print_r($do);
-                echo "</pre>";
                 $akum_diskon = 0;
                 $no += 1;
                 $jumlah = $do['qty'];
