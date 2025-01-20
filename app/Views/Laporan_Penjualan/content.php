d3840a76c8a0ae30b019526a3d6f6eb690fe3d87
Luhur Gunawan <loehur@gmail.com>
2025-01-20 11:20:35 +0700

    oke dehssws2s3s2222s2s2222s222224s2

--- app/Views/Laporan_Penjualan/content.php
+++ app/Views/Laporan_Penjualan/content.php
@@ -1,18 +1,6 @@
 <main>
     <?php $wdth = 135; ?>
-    <div class="ms-3 mt-3 me-3 bg-white">
-        <div class="row">
-            <div class="col">
-                <form target="_blank" action="<?php PV::BASE_URL ?>Laporan_Penjualan/cek_data" method="POST">
-                    <div class="input-group">
-                        <span class="input-group-text bg-white text-primary" style="width: <?= $wdth ?>px;">Data</span>
-                        <input name="from" type="date" min="2023-07-01" style="max-width: 140px;" max="<?= date("Y-m-d") ?>" placeholder="YYYY-MM-DD" class="form-control" required>
-                        <input name="to" type="date" min="2023-07-01" max="<?= date("Y-m-d") ?>" placeholder="YYYY-MM-DD" class="form-control" required>
-                        <button type="submit" class="btn bg-gradient btn-primary">Cek</button>
-                    </div>
-                </form>
-            </div>
-        </div>
+    <div class="ms-3 me-3 bg-white">
         <div class="row">
             <div class="col">
                 <form target="_blank" action="<?php PV::BASE_URL ?>Laporan_Penjualan/cek_rekap_1" method="POST">
@@ -38,4 +26,32 @@
             </div>
         </div>
     </div>
+    <div id="data"></div>
 </main>
+
+<script src="<?= PV::ASSETS_URL ?>js/jquery-3.7.0.min.js"></script>
+<script>
+    $("form").on("submit", function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: $(this).attr('action'),
+            data: $(this).serialize(),
+            type: $(this).attr("method"),
+            success: function(res) {
+                $("div#data").html(res);
+            },
+        });
+    });
+
+    $(document).ready(function() {
+        $('#dt_tb').dataTable({
+            "bLengthChange": false,
+            "bFilter": true,
+            "bInfo": false,
+            "bAutoWidth": false,
+            "pageLength": 50,
+            "scrollY": 600,
+            "dom": "lfrti"
+        });
+    })
+</script>
--- app/Views/Laporan_Penjualan/rekap.php
+++ app/Views/Laporan_Penjualan/rekap.php
@@ -1,48 +1,7 @@
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=500, user-scalable=no">
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Orins | Rekap Penjualan</title>
-    <link href="<?= PV::ASSETS_URL ?>css/styles.css" rel="stylesheet" />
-    <link rel="stylesheet" href="<?= PV::ASSETS_URL ?>plugins/bootstrap-5.1/bootstrap.min.css" rel="stylesheet" />
-    <link rel="icon" type="image/x-icon" href="<?= PV::ASSETS_URL ?>assets/img/favicon.png" />
-    <script src="<?= PV::ASSETS_URL ?>js/feather.min.js" crossorigin="anonymous"></script>
-
-    <link rel="stylesheet" href="<?= PV::ASSETS_URL ?>plugins/fontawesome-free-6.4.0-web/css/all.css" rel="stylesheet">
-    <link href="<?= PV::ASSETS_URL ?>plugins/toggle/css/bootstrap-toggle.min.css" rel="stylesheet">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web&display=swap" rel="stylesheet">
-    <!-- FONT -->
-    <?php $fontStyle = "'Titillium Web', sans-serif;" ?>
-</head>
-
+<link rel="stylesheet" href="<?= PV::ASSETS_URL ?>css/dataTables.dataTables.min.css" rel="stylesheet" />
 <style>
-    html {
-        height: 100%;
-    }
-
-    html .table {
-        font-family: <?= $fontStyle ?>;
-    }
-
-    html .content {
-        font-family: <?= $fontStyle ?>;
-    }
-
-    html body {
-        font-family: <?= $fontStyle ?>;
-    }
-
-    main {
-        margin-bottom: 20px;
-    }
-
-    .col-t {
-        line-height: 100%;
+    .dt-search {
+        float: right !important;
     }
 </style>
 
@@ -52,7 +11,7 @@
             <?= $data['range']['from'] ?> s/d <?= $data['range']['to'] ?>
         </div>
     </div>
-    <table class="table table-sm">
+    <table class="table table-sm text-sm" id="dt_tb">
         <tr>
             <th>Item Produksi</th>
             <th class="text-end">Qty</th>
@@ -69,3 +28,18 @@
         ?>
     </table>
 </main>
+
+<script src="<?= PV::ASSETS_URL ?>js/dataTables.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#tb_dt').dataTable({
+            "bLengthChange": false,
+            "bFilter": true,
+            "bInfo": false,
+            "bAutoWidth": false,
+            "pageLength": 50,
+            "scrollY": 600,
+            "dom": "lfrti"
+        });
+    })
+</script>
