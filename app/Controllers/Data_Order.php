ff1d078dea931a965655d31d0b764f3f232294df
Luhur Gunawan <loehur@gmail.com>
2025-01-20 09:56:39 +0700

    oke dehssws2s3s2222s2s

--- app/Controllers/Data_Order.php
+++ app/Controllers/Data_Order.php
@@ -219,9 +219,9 @@ class Data_Order extends Controller
             }
             if ($do['price_locker'] == 1) {
                if (!isset($data['paket'][$do['paket_ref']]['qty'])) {
-                  echo "cek";
                   $get = $this->db(0)->get_where_row('paket_order', "paket_ref = '" . $do['paket_ref'] . "' AND price_locker = 1");
                   if (isset($get['qty'])) {
+                     echo "cek";
                      $data['paket'][$do['paket_ref']]['qty'] = $do['jumlah'] / $get['qty'];
                   }
                }
