b436ea971c03e1c8eff77a9b23edd9190b502821
Luhur Gunawan <loehur@gmail.com>
2025-01-20 14:33:04 +0700

    add supplier

---
+++ app/Controllers/Gudang_Supplier.php
@@ -0,0 +1,52 @@
+<?php
+
+class Gudang_Supplier extends Controller
+{
+   public $main_table = "pelanggan";
+
+   public function __construct()
+   {
+      $this->session_cek();
+      $this->data_order();
+      if (!in_array($this->userData['user_tipe'], PV::PRIV[7])) {
+         $this->model('Log')->write($this->userData['user'] . " Force Logout. Hacker!");
+         $this->logout();
+      }
+
+      $this->v_content = __CLASS__ . "/content";
+      $this->v_viewer = "Layouts/viewer";
+   }
+
+   public function index()
+   {
+
+      $this->view("Layouts/layout_main", [
+         "content" => $this->v_content,
+         "title" => "Gudang - Supplier"
+      ]);
+      $this->viewer();
+   }
+
+   public function viewer($parse = "")
+   {
+      $this->view($this->v_viewer, ["controller" => __CLASS__, "parse" => $parse]);
+   }
+
+   public function content($parse = "1")
+   {
+      $data['supplier'] = $this->db(0)->get('master_supplier');
+      $this->view($this->v_content, $data);
+   }
+
+   function add()
+   {
+      $name = $_POST['nama'];
+      $id = substr($name, 0, 1) . date("ymd") . rand(0, 9);
+      $do = $this->db(0)->insertCols("master_supplier", "id, nama", "'" . $id . "','" . strtoupper($name) . "'");
+      if ($do['errno'] <> 0) {
+         echo $do['error'];
+      } else {
+         echo 0;
+      }
+   }
+}
---
+++ app/Views/Gudang_Supplier/content.php
@@ -0,0 +1,61 @@
+<main>
+    <div class="row mx-0">
+        <div class="col">
+            <span class="fw-bold">Supplier</span>
+            <button type="button" class="float-end btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Tambah</button>
+
+            <table class="table table-sm">
+                <?php
+                foreach ($data['supplier'] as $a) { ?>
+                    <tr>
+                        <td><?= $a['id'] ?></td>
+                        <td><?= $a['nama'] ?></td>
+                    </tr>
+                <?php }
+                ?>
+            </table>
+        </div>
+    </div>
+</main>
+
+<div class="modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Menambah Supplier</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="<?= PV::BASE_URL ?>Gudang_Supplier/add" method="POST">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label class="form-label" required>Nama Supplier</label>
+                        <input type="text" name="nama" class="form-control" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                    <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">Tambah</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script src="<?= PV::ASSETS_URL ?>js/jquery-3.7.0.min.js"></script>
+<script>
+    $("form").on("submit", function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: $(this).attr('action'),
+            data: $(this).serialize(),
+            type: $(this).attr("method"),
+            success: function(result) {
+                if (result == 0) {
+                    content();
+                } else {
+                    alert(result);
+                }
+            },
+        });
+    });
+</script>
