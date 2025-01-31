6a00271e5f17214aa694fb6fa3d5cc37df029daa
Luhur Gunawan <loehur@gmail.com>
2025-01-31 15:56:58 +0700

    export yesddd

--- app/Controllers/Export.php
+++ app/Controllers/Export.php
@@ -6,7 +6,7 @@ class Export extends Controller
    {
       $this->session_cek();
       $this->data_order();
-      if (!in_array($this->userData['user_tipe'], PV::PRIV[8])) {
+      if (!in_array($this->userData['user_tipe'], PV::PRIV[8]) && !in_array($this->userData['user_tipe'], PV::PRIV[6])) {
          $this->model('Log')->write($this->userData['user'] . " Force Logout. Hacker!");
          $this->logout();
       }
