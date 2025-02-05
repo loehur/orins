14d60fbb7057b3ae444894bcef8a56116b9b4c6a
Luhur Gunawan <loehur@gmail.com>
2025-02-05 16:22:32 +0700

    gas fasdfs222

--- app/Data/Barang.php
+++ app/Data/Barang.php
@@ -108,38 +108,4 @@ class Barang extends Controller
             return 0;
         }
     }
-
-
-    function stok_data_proses($kode, $id_toko)
-    {
-        $cols = "id_barang, CONCAT('U',sn,sds) as unic, sn, sds, sum(qty) as qty";
-        $where_masuk = "id_barang = '" . $kode . "' AND id_target = '" . $id_toko . "' AND stat = 1 GROUP BY sn, sds";
-        $where_keluar = "id_barang = '" . $kode . "' AND id_sumber = '" . $id_toko . "' AND stat = 1 GROUP BY sn, sds";
-
-        $masuk = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_masuk, 1, "unic");
-        $keluar = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_keluar, 1, "unic");
-
-        foreach ($masuk as $key => $ms) {
-            if (isset($keluar[$key])) {
-                $masuk[$key]['qty'] -= $keluar[$key]['qty'];
-            }
-        }
-
-        return $masuk;
-    }
-
-    function cek_proses($kode, $id_toko, $sn, $sds, $qty)
-    {
-        $stok = $this->stok_data_proses($kode, $id_toko);
-        $unic = "U" . $sn . $sds;
-        if (isset($stok[$unic])) {
-            if ($stok[$unic]['qty'] < $qty) {
-                return 0;
-            }
-        } else {
-            return 0;
-        }
-
-        return 1;
-    }
 }
