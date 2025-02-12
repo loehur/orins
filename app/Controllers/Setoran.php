5c2f6b02c91ae85757c405e4dcc2f9f2672ae2d7
Luhur Gunawan <loehur@gmail.com>
2025-02-12 08:13:09 +0700

    surcharge332333

--- app/Controllers/Setoran.php
+++ app/Controllers/Setoran.php
@@ -103,10 +103,6 @@ class Setoran extends Controller
       $where = "id_toko = " . $this->userData['id_toko'] . " AND metode_mutasi = 1 AND id_client <> 0 AND ref_setoran = ''";
       $data['kas_trx'] = $this->db(0)->get_where('kas', $where, 'ref_transaksi', 1);
 
-      $nontunai_sds = 0;
-      $where_kas_sds = "id_toko = " . $this->userData['id_toko'] . " AND metode_mutasi = 2 AND sds = 1 AND id_client <> 0 AND ref_setoran = ''";
-      $nontunai_sds = $this->db(0)->sum_col_where('kas', 'jumlah', $where_kas_sds);
-
       $ref_trx = array_keys($data['kas_trx']);
       $reft_list = "";
       foreach ($ref_trx as $r) {
@@ -115,6 +111,10 @@ class Setoran extends Controller
       $reft_list = rtrim($reft_list, ',');
       $where_ref = "ref IN (" . $reft_list . ") AND sds = 1";
       $data['sds'] = $this->db(0)->get_where('master_mutasi', $where_ref, 'ref');
+
+      $nontunai_sds = 0;
+      $where_kas_sds = "ref_transaksi IN (" . $reft_list . ") AND sds = 1";
+      $nontunai_sds = $this->db(0)->sum_col_where('kas', 'jumlah', $where_kas_sds);
 
       $total_sds = 0;
       foreach ($data['sds'] as $ds) {
