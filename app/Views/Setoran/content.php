6b6532ff44707b0b60d4d3dba5c0a41fd6787ad8
Luhur Gunawan <loehur@gmail.com>
2025-02-20 09:12:25 +0700

    gas df

--- app/Views/Setoran/content.php
+++ app/Views/Setoran/content.php
@@ -102,7 +102,7 @@
     <?php
     $total_sds = 0;
     foreach ($data['sds'] as $ds) {
-        echo (($ds['harga_jual'] - $ds['diskon']) * $ds['qty']) . ",";
+        echo $ds['harga_jual'] . ",";
         $total_sds += (($ds['harga_jual'] - $ds['diskon']) * $ds['qty']);
     }
 
