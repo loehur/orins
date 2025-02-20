5013fe43f43016cfbd2a25888a61f3b6647df26d
Luhur Gunawan <loehur@gmail.com>
2025-02-20 08:33:09 +0700

    gas keun3542

--- app/Controllers/Setoran.php
+++ app/Controllers/Setoran.php
@@ -53,7 +53,7 @@ class Setoran extends Controller
          $where_ref = "ref IN (" . $reft_list . ") AND sds = 1 AND stat = 1 AND jenis = 2 AND id_sumber = '" . $this->userData['id_toko'] . "'";
          $data['sds'] = $this->db(0)->get_where('master_mutasi', $where_ref, 'ref');
 
-         $where_kas_sds = "ref_transaksi IN (" . $reft_list . ") AND sds = 1 AND metode_mutasi = 2 AND status_mutasi <> 2";
+         $where_kas_sds = "ref_transaksi IN (" . $reft_list . ") AND sds = 1 AND metode_mutasi <> 1 AND status_mutasi <> 2";
          $data['nontunai_sds'] = $this->db(0)->sum_col_where('kas', 'jumlah', $where_kas_sds);
       } else {
          $data['sds'] = [];
@@ -101,7 +101,7 @@ class Setoran extends Controller
       $ref = date("ymdhis") . rand(0, 9);
       $set = "ref_setoran = '" . $ref . "'";
 
-      $where = "id_toko = " . $this->userData['id_toko'] . " AND metode_mutasi = 1 AND id_client <> 0 AND ref_setoran = ''";
+      $where = "id_toko = " . $this->userData['id_toko'] . " AND id_client <> 0 AND ref_setoran = ''";
       $data['kas_trx'] = $this->db(0)->get_where('kas', $where, 'ref_transaksi', 1);
 
       $ref_trx = array_keys($data['kas_trx']);
@@ -114,7 +114,7 @@ class Setoran extends Controller
       $data['sds'] = $this->db(0)->get_where('master_mutasi', $where_ref, 'ref');
 
       $nontunai_sds = 0;
-      $where_kas_sds = "ref_transaksi IN (" . $reft_list . ") AND sds = 1 AND status_mutasi <> 2";
+      $where_kas_sds = "ref_transaksi IN (" . $reft_list . ") AND metode_mutasi <> 1 AND sds = 1 AND status_mutasi <> 2";
       $nontunai_sds = $this->db(0)->sum_col_where('kas', 'jumlah', $where_kas_sds);
 
       $total_sds = 0;
