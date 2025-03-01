2880266a4a4de72786b27393bb536f97deea513a
Luhur Gunawan <loehur@gmail.com>
2025-03-01 10:18:29 +0700

    cicil2 setor kas kecil

--- app/Controllers/Audit_KasKecil.php
+++ app/Controllers/Audit_KasKecil.php
@@ -31,7 +31,7 @@ class Audit_KasKecil extends Controller
 
    public function content()
    {
-      $whereSplit = "id_target = 1 AND tipe = 0 AND st = 0 AND id_sumber = " . $this->userData['id_toko'];
+      $whereSplit = "id_target = 1 AND tipe = 0 AND st = 0 AND id_sumber = " . $this->userData['id_toko'] . " AND ref_setoran = ''";
       $data['split'] = $this->db(0)->get_where('kas_kecil', $whereSplit, 'ref');
 
       $whereSplit = "id_target = 1 AND tipe = 0 AND st <> 0 AND id_sumber = " . $this->userData['id_toko'];
@@ -61,8 +61,9 @@ class Audit_KasKecil extends Controller
       $where = "id_toko = " . $this->userData['id_toko'] . " AND metode_mutasi = 1 AND jenis_transaksi = 3 AND status_setoran = 0 AND status_mutasi = 1 ORDER BY id_kas DESC";
       $data['pengeluaran'] = $this->db(0)->get_where('kas', $where, 'ref_setoran', 1);
 
-      $where = "id_toko = " . $this->userData['id_toko'] . " AND metode_mutasi = 1 AND jenis_transaksi = 3 AND status_setoran <> 0 ORDER BY id_kas DESC";
-      $data['pengeluaran_done'] = $this->db(0)->get_where('kas', $where, 'ref_setoran', 1);
+      $data['pengeluaran_done'] = [];
+      // $where = "id_toko = " . $this->userData['id_toko'] . " AND metode_mutasi = 1 AND jenis_transaksi = 3 AND status_setoran <> 0 ORDER BY id_kas DESC";
+      // $data['pengeluaran_done'] = $this->db(0)->get_where('kas', $where, 'ref_setoran', 1);
       $this->view(__CLASS__ . '/content', $data);
    }
 
@@ -107,5 +108,27 @@ class Audit_KasKecil extends Controller
       $where = "id = '" . $id . "'";
       $update = $this->db(0)->update("kas_kecil", $set, $where);
       echo $update['errno'] == 0 ? 0 : $update['error'];
+   }
+
+   function setor()
+   {
+      $ref = date("ymdhis") . rand(0, 9);
+      $set = "ref_setoran = '" . $ref . "'";
+
+      $where = "(tipe = 0) AND st <> 0 AND id_sumber = " . $this->userData['id_toko'] . " AND ref_setoran = ''";
+      $update = $this->db(0)->update("kas_kecil", $set, $where);
+      if ($update['errno'] <> 0) {
+         echo $update['error'];
+         exit();
+      }
+
+      $where = "(tipe = 5) AND id_sumber = " . $this->userData['id_toko'] . " AND ref_setoran = ''";
+      $update = $this->db(0)->update("kas_kecil", $set, $where);
+      if ($update['errno'] <> 0) {
+         echo $update['error'];
+         exit();
+      }
+
+      echo 0;
    }
 }
--- app/Views/Audit_KasKecil/content.php
+++ app/Views/Audit_KasKecil/content.php
@@ -66,35 +66,37 @@
             <?php } ?>
         </table>
 
-        <table class="table table-sm text-sm">
-            <tr>
-                <th colspan="10" class="text-danger">Riwayat Pengeluaran</th>
-            </tr>
-            <?php foreach ($data['pengeluaran_done'] as $ref => $keluar) { ?>
-                <?php
-                $jumlah_keluar = 0;
-                foreach ($keluar as $a) { ?>
-                    <tr>
-                        <td>
-                            #<?= $a['id_kas'] ?>
-                        </td>
-                        <td>
-                            <?= date('d/m/y H:i', strtotime($a['insertTime'])) ?>
-                        </td>
-                        <td>
-                            <?= $data['jkeluar'][$a['ref_transaksi']]['nama'] ?>
-                        </td>
-                        <td>
-                            <?= $a['note'] ?>
-                        </td>
-                        <td class="text-end">
-                            <?= number_format($a['jumlah']) ?>
-                        </td>
-                        <td><a class="ajax" href="<?= PV::BASE_URL ?>Audit_KasKecil/reimburse/<?= $a['id_kas'] ?>"><span class="badge bg-primary ">Reimburse</span></a></td>
-                    </tr>
+        <?php if (count($data['pengeluaran_done']) > 0) { ?>
+            <table class="table table-sm text-sm">
+                <tr>
+                    <th colspan="10" class="text-danger">Riwayat Pengeluaran</th>
+                </tr>
+                <?php foreach ($data['pengeluaran_done'] as $ref => $keluar) { ?>
+                    <?php
+                    $jumlah_keluar = 0;
+                    foreach ($keluar as $a) { ?>
+                        <tr>
+                            <td>
+                                #<?= $a['id_kas'] ?>
+                            </td>
+                            <td>
+                                <?= date('d/m/y H:i', strtotime($a['insertTime'])) ?>
+                            </td>
+                            <td>
+                                <?= $data['jkeluar'][$a['ref_transaksi']]['nama'] ?>
+                            </td>
+                            <td>
+                                <?= $a['note'] ?>
+                            </td>
+                            <td class="text-end">
+                                <?= number_format($a['jumlah']) ?>
+                            </td>
+                            <td><a class="ajax" href="<?= PV::BASE_URL ?>Audit_KasKecil/reimburse/<?= $a['id_kas'] ?>"><span class="badge bg-primary ">Reimburse</span></a></td>
+                        </tr>
+                    <?php } ?>
                 <?php } ?>
-            <?php } ?>
-        </table>
+            </table>
+        <?php } ?>
 
         <table class="table table-sm text-sm">
             <tr>
@@ -125,7 +127,7 @@
             <div class="col text-end fw-bold pt-2">
                 Total Saldo <?= number_format($data['setor']) ?>
             </div>
-            <div class="col"><span class="btn btn-primary">Setor</span></div>
+            <div class="col"><span id="setor" class="btn btn-primary">Setor</span></div>
         </div>
     </div>
 </main>
@@ -133,6 +135,21 @@
 <script src="<?= PV::ASSETS_URL ?>js/jquery-3.7.0.min.js"></script>
 
 <script>
+    $("#setor").dblclick(function() {
+        $.ajax({
+            url: "<?= PV::BASE_URL ?>Audit_KasKecil/setor",
+            data: [],
+            type: "POST",
+            success: function(result) {
+                if (result == 0) {
+                    content();
+                } else {
+                    alert(result);
+                }
+            },
+        });
+    })
+
     $("a.ajax").click(function(e) {
         e.preventDefault();
         var href = $(this).attr('href');
