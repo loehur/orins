0815f6a4d5dc634159fb7b7f968334d0a86ec37b
Luhur Gunawan <loehur@gmail.com>
2025-05-05 10:53:39 +0700

    fix ref bug rady33

--- app/Views/Data_Operasi/content.php
+++ app/Views/Data_Operasi/content.php
@@ -559,7 +559,7 @@
                                                         </td>
                                                         <?php if ($ada_produksi[$ref] == true) { ?>
                                                             <td>
-                                                                <?php if ($this->userData['id_toko'] == $id_toko) { ?>
+                                                                <?php if ($this->userData['id_toko'] == $id_toko[$ref]) { ?>
                                                                     <?= $data['ref'][$ref]['ready_cs'] ?>
                                                                     <?php if (isset($data['karyawan'][$data['ref'][$ref]['ready_cs']])) { ?>
                                                                         &nbsp;<span class="text-sm"><i class="fa-solid fa-check-double"></i> <?= $data['karyawan'][$data['ref'][$ref]['ready_cs']]['nama'] ?></span>
