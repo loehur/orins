3dd21a1f06927c5779163e2ee88dde360c674f6c
Luhur Gunawan <loehur@gmail.com>
2025-05-05 08:58:37 +0700

    bug cs to4

--- app/Controllers/Data_Operasi.php
+++ app/Controllers/Data_Operasi.php
@@ -140,17 +140,18 @@ class Data_Operasi extends Controller
 
       foreach ($data['order'] as $ref => $do) {
          foreach ($do as $dd) {
-            $data['head'][$ref]['cs'] = $dd['id_penerima'];
-            if ($dd['id_user_afiliasi'] <> 0) {
-               $data['head'][$ref]['cs_to'] = $dd['id_user_afiliasi'];
+            if ($dd['cancel'] == 0) {
+               $data['head'][$ref]['cs'] = $dd['id_penerima'];
+               if ($dd['id_user_afiliasi'] <> 0) {
+                  $data['head'][$ref]['cs_to'] = $dd['id_user_afiliasi'];
+               }
+               if ($dd['id_afiliasi'] <> 0) {
+                  $data['head'][$ref]['id_afiliasi'] = $dd['id_afiliasi'];
+               }
+               $data['head'][$ref]['insertTime'] = $dd['insertTime'];
+               $data['head'][$ref]['tuntas'] = $dd['tuntas'];
+               $data['head'][$ref]['user_id'] = $dd['id_user'];
             }
-            if ($dd['id_afiliasi'] <> 0) {
-               $data['head'][$ref]['id_afiliasi'] = $dd['id_afiliasi'];
-            }
-            $data['head'][$ref]['insertTime'] = $dd['insertTime'];
-            $data['head'][$ref]['tuntas'] = $dd['tuntas'];
-            $data['head'][$ref]['user_id'] = $dd['id_user'];
-            break;
          }
       }
 
