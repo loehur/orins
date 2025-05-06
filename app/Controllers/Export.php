8d0548cc7d6b0973b9e6155ec7ad8ff7c1daa886
Luhur Gunawan <loehur@gmail.com>
2025-05-06 09:43:30 +0700

    to toko aff small4443434444

--- app/Controllers/Export.php
+++ app/Controllers/Export.php
@@ -6,7 +6,7 @@ class Export extends Controller
    {
       $this->session_cek();
       $this->data_order();
-      if (!in_array($this->userData['user_tipe'], PV::PRIV[8]) && !in_array($this->userData['user_tipe'], PV::PRIV[6])) {
+      if (!in_array($this->userData['user_tipe'], PV::PRIV[100])) {
          $this->model('Log')->write($this->userData['user'] . " Force Logout. Hacker!");
          $this->logout();
       }
