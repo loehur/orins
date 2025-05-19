94a609ecd2331e568eb7731b8f75ac012da2a3d7
Luhur Gunawan <loehur@gmail.com>
2025-05-19 15:21:17 +0700

    jalur cepat4455555545555

--- app/Controllers/Tool.php
+++ app/Controllers/Tool.php
@@ -5,9 +5,13 @@ class Tool extends Controller
    function mutasi() //davinci aff to davinci rekanan
    {
       $id_rekanan = 1245;
+      $pelanggan = $this->db(0)->get('pelanggan', 'id_pelanggan');
       $cek = $this->db(0)->get_where("order_data", "id_toko = 4");
+
       echo "<pre>";
-      print_r($cek);
+      foreach ($cek as $c) {
+         echo $pelanggan[$c['id_pelanggan']]['nama'] . "<br>";
+      }
       echo "</pre>";
    }
 }
