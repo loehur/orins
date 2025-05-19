2914fd78bfe8f1062285fe698eaacc10136d5304
Luhur Gunawan <loehur@gmail.com>
2025-05-19 15:49:17 +0700

    mutasi davinci2555554

--- app/Controllers/Tool.php
+++ app/Controllers/Tool.php
@@ -5,13 +5,20 @@ class Tool extends Controller
    function mutasi() //davinci aff to davinci rekanan
    {
       $id_rekanan = 1245;
-      $pelanggan = $this->db(0)->get('pelanggan', 'id_pelanggan');
-      $cek = $this->db(0)->get_where("order_data", "id_toko = 4 AND cancel = 0 AND id_afiliasi <> 0 AND id_pelanggan <> 0",);
+      $pelanggan = $this->db(0)->get_where('pelanggan', 'id_toko = 2', 'id_pelanggan');
+      $cek = $this->db(0)->get_where("order_data", "id_toko = 2 AND cancel = 0 AND id_afiliasi <> 0 AND id_pelanggan <> 0 AND insertTime LIKE '2025-05%'",);
 
       echo "<pre>";
       foreach ($cek as $k => $c) {
          $id[$k] = $c['id_order_data'];
-         $pelanggan[$k] = $pelanggan[$c['id_pelanggan']]['nama'];
+
+         if (isset($pelanggan[$c['id_pelanggan']])) {
+            $pelanggan[$k] = $pelanggan[$c['id_pelanggan']]['nama'];
+         } else {
+            echo $pelanggan[$c['id_pelanggan']] . " ";
+            continue;
+         }
+
          $ref[$k] = $c['ref'];
          $cs_id[$k] = $c['id_user_afiliasi'];
          $id_toko[$k] = $c['id_afiliasi'];
@@ -37,12 +44,12 @@ class Tool extends Controller
          //    echo $pelanggan[$k] . " ID TOKO OK<br>";
          // }
 
-         $up = $this->db(0)->update("order_data", "id_pelanggan = " . $id_rekanan, "id_order_data = " . $id[$k]);
-         if ($up['errno'] <> 0) {
-            echo $up['error'] . "<br>";
-         } else {
-            echo $pelanggan[$k] . " OK<br>";
-         }
+         // $up = $this->db(0)->update("order_data", "id_pelanggan = " . $id_rekanan, "id_order_data = " . $id[$k]);
+         // if ($up['errno'] <> 0) {
+         //    echo $up['error'] . "<br>";
+         // } else {
+         //    echo $pelanggan[$k] . " OK<br>";
+         // }
       }
       echo "</pre>";
    }
