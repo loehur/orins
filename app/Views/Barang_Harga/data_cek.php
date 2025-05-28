c6d68832713789208d23a555b7a41aec7905c87f
Luhur Gunawan <loehur@gmail.com>
2025-05-28 16:55:59 +0700

    warna44

--- app/Views/Barang_Harga/data_cek.php
+++ app/Views/Barang_Harga/data_cek.php
@@ -20,7 +20,7 @@
     <br>
     <label class="border border-bottom-0 bg-danger text-white px-2">Gudang</label>
     <div class="border px-1 py-2">
-        <?php if (count($data['stok']) == 0) { ?>
+        <?php if (count($data['stok_gudang']) == 0) { ?>
             <div class="w-100 text-center text-sm">Kosong</div>
         <?php } else { ?>
             <table class="table table-sm mb-0 text-sm">
