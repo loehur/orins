3473ec0f308ba2fd96f4a13448c16bc8c8105236
Luhur Gunawan <loehur@gmail.com>
2025-05-28 16:55:47 +0700

    warna4

--- app/Views/Barang_Harga/data_cek.php
+++ app/Views/Barang_Harga/data_cek.php
@@ -21,20 +21,19 @@
     <label class="border border-bottom-0 bg-danger text-white px-2">Gudang</label>
     <div class="border px-1 py-2">
         <?php if (count($data['stok']) == 0) { ?>
-
+            <div class="w-100 text-center text-sm">Kosong</div>
         <?php } else { ?>
-
+            <table class="table table-sm mb-0 text-sm">
+                <?php foreach ($data['stok_gudang'] as $key => $d) {
+                    if ($d['qty'] == 0) {
+                        continue;
+                    } ?>
+                    <tr>
+                        <td><?= $d['sds'] == 1 ? "SDS" : "" ?></td>
+                        <td class="text-end"><?= $d['sn'] ?></td>
+                    </tr>
+                <?php } ?>
+            </table>
         <?php } ?>
-        <table class="table table-sm mb-0 text-sm">
-            <?php foreach ($data['stok_gudang'] as $key => $d) {
-                if ($d['qty'] == 0) {
-                    continue;
-                } ?>
-                <tr>
-                    <td><?= $d['sds'] == 1 ? "SDS" : "" ?></td>
-                    <td class="text-end"><?= $d['sn'] ?></td>
-                </tr>
-            <?php } ?>
-        </table>
     </div>
 </div>
