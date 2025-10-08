df21c762fe76552500b049ae5c2835fc43bb5e7f
Luhur Gunawan <loehur@gmail.com>
2025-10-08 12:35:05 +0700

    export paketr45

--- app/Controllers/Export.php
+++ app/Controllers/Export.php
@@ -6,7 +6,7 @@ class Export extends Controller
    {
       $this->session_cek();
       $this->data_order();
-      if (!in_array($this->userData['user_tipe'], PV::PRIV[100])) {
+      if (!in_array($this->userData['user_tipe'], PV::PRIV[107])) {
          $this->model('Log')->write($this->userData['user'] . " Force Logout. Hacker!");
          $this->logout();
       }
