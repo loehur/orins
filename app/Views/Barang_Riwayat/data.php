f42affdef3cb8417f1a6839878ab6728c6391924
Luhur Gunawan <loehur@gmail.com>
2025-10-14 14:06:32 +0700

    alriwayat

--- app/Views/Barang_Riwayat/data.php
+++ app/Views/Barang_Riwayat/data.php
@@ -30,7 +30,7 @@
                             echo '<i class="fa-solid fa-arrow-up text-danger"></i>';
                             break;
                         case 4:
-                            echo '<i class="fa-solid fa-arrow-up text-danger"></i>';
+                            echo '<i class="fa-solid fa-arrow-up text-warning"></i>';
                             break;
                         case 3:
                             if ($d['id_target'] == 0) {
