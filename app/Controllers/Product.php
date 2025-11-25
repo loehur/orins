6fd82e96facc2c107479b2294b3ad1d8637b687b
Luhur Gunawan <loehur@gmail.com>
2025-11-25 16:37:08 +0700

    jadi453556

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -5,17 +5,18 @@ class Product extends Controller
    function get()
    {
       $where = "en = 1 AND model != '' ";
-      $products = $this->db(0)->get_cols_where('master_barang', ('id, CONCAT(brand, model) as nama_barang'), $where, 1, 'id');
+      $products = $this->db(0)->get_cols_where('master_barang', ('id, CONCAT(brand, " ", model) as nama_barang'), $where, 1, 'id');
       echo json_encode($products, JSON_UNESCAPED_UNICODE);
    }
 
    function getStock()
    {
       $where = "en = 1 AND model != '' ";
-      $barang = $this->db(0)->get_cols_where('master_barang', ('id, CONCAT(brand, model) as nama_barang, harga_1 as price'), $where, 1, 'id');
+      $barang = $this->db(0)->get_cols_where('master_barang', ('id, CONCAT(brand, " ", model) as nama_barang, harga_1 as price'), $where, 1, 'id');
       $stok = $this->data('Barang')->stok_data_web();
-
-      $hasil = array_merge_recursive($barang, $stok);
-      echo json_encode($hasil, JSON_UNESCAPED_UNICODE);
+      foreach ($stok as $key => $s) {
+         $stok[$key]['price'] = $barang[$key]['price'];
+      }
+      echo json_encode($stok, JSON_UNESCAPED_UNICODE);
    }
 }
