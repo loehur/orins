e9d5ca0171d02ad4d3e21926be8bb69895471203
Luhur Gunawan <loehur@gmail.com>
2025-11-25 16:41:34 +0700

    jadi453556trt

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -11,12 +11,12 @@ class Product extends Controller
 
    function getStock()
    {
-      $where = "en = 1 AND model != '' ";
+      $where = "en = 1 AND model <> ''";
       $barang = $this->db(0)->get_cols_where('master_barang', ('id, harga_1'), $where, 1, 'id');
       $stok = $this->data('Barang')->stok_data_web();
       // foreach ($stok as $key => $s) {
       //    $stok[$key]['price'] = $barang[$key]['harga_1'];
       // }
-      echo json_encode($stok, JSON_UNESCAPED_UNICODE);
+      echo json_encode($barang, JSON_UNESCAPED_UNICODE);
    }
 }
