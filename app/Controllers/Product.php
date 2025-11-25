b8b10e8d8550ec57244c29f4e9da7ce560bd3ce4
Luhur Gunawan <loehur@gmail.com>
2025-11-25 13:15:48 +0700

    gas

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -6,6 +6,6 @@ class Product extends Controller
    {
       $where = "en = 1";
       $products = $this->db(0)->get_cols_where('master_barang', ('id, CONCAT(brand, model) as nama_barang'), $where, 1, 'id');
-      echo "[" . json_encode($products, JSON_UNESCAPED_UNICODE) . "]";
+      echo json_encode($products, JSON_UNESCAPED_UNICODE);
    }
 }
