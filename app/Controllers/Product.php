aaaf5f7cee2463849d9674a273b8c713bf5cc5b2
Luhur Gunawan <loehur@gmail.com>
2025-11-25 16:38:05 +0700

    jadi453556t

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -12,10 +12,10 @@ class Product extends Controller
    function getStock()
    {
       $where = "en = 1 AND model != '' ";
-      $barang = $this->db(0)->get_cols_where('master_barang', ('id, CONCAT(brand, " ", model) as nama_barang, harga_1 as price'), $where, 1, 'id');
+      $barang = $this->db(0)->get_cols_where('master_barang', ('id, harga_1'), $where, 1, 'id');
       $stok = $this->data('Barang')->stok_data_web();
       foreach ($stok as $key => $s) {
-         $stok[$key]['price'] = $barang[$key]['price'];
+         $stok[$key]['price'] = $barang[$key]['harga_1'];
       }
       echo json_encode($stok, JSON_UNESCAPED_UNICODE);
    }
