022012091fc2d5add736e25745e93add0ca2b3fb
Luhur Gunawan <loehur@gmail.com>
2026-01-27 15:26:27 +0700

    okee

--- app/Data/Barang.php
+++ app/Data/Barang.php
@@ -4,25 +4,29 @@ class Barang extends Controller
 {
     function stok_data($kode, $id_toko)
     {
-        $cols = "id_barang, CONCAT('U',sn,sds) as unic, sn, sds, sum(qty) as qty";
-        $where_masuk = "id_barang = '" . $kode . "' AND id_target = '" . $id_toko . "' AND stat = 1 GROUP BY sn, sds";
-        $where_keluar = "id_barang = '" . $kode . "' AND id_sumber = '" . $id_toko . "' AND stat <> 2 GROUP BY sn, sds";
-        $where_cart = "id_barang = '" . $kode . "' AND id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2 GROUP BY sn, sds";
+        $cols = "id_barang, CONCAT('U',sn,sds) as unic, sn, sds, 
+            (SUM(CASE WHEN id_target = '" . $id_toko . "' AND stat = 1 THEN qty ELSE 0 END) - 
+            SUM(CASE WHEN id_sumber = '" . $id_toko . "' AND stat <> 2 THEN qty ELSE 0 END)) as qty,
+            SUM(CASE WHEN id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2 THEN qty ELSE 0 END) as cart";
 
-        $masuk = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_masuk, 1, "unic");
-        $keluar = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_keluar, 1, "unic");
-        $cart = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_cart, 1, "unic");
+        $where = "id_barang = '" . $kode . "' AND 
+            ((id_target = '" . $id_toko . "' AND stat = 1) OR 
+            (id_sumber = '" . $id_toko . "' AND stat <> 2) OR 
+            (id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2)) 
+            GROUP BY sn, sds";
+
+        $masuk = $this->db(0)->get_cols_where('master_mutasi', $cols, $where, 1, "unic");
+
+        $cart_list_cache = null;
 
         foreach ($masuk as $key => $ms) {
-            if (isset($keluar[$key])) {
-                $masuk[$key]['qty'] -= $keluar[$key]['qty'];
-            }
-            if (isset($cart[$key])) {
-                $masuk[$key]['cart'] = $cart[$key]['qty'];
-
-                $cols = "id_barang, user_id, CONCAT('U',sn,sds,user_id) as unic, sn, sds, sum(qty) as qty";
-                $where_cart_list = "id_barang = '" . $kode . "' AND id_sumber = '" . $id_toko . "' AND stat = 0 GROUP BY sn, sds, user_id";
-                $masuk[$key]['cart_list'] = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_cart_list);
+            if ($masuk[$key]['cart'] > 0) {
+                if ($cart_list_cache === null) {
+                    $cols_cart = "id_barang, user_id, CONCAT('U',sn,sds,user_id) as unic, sn, sds, sum(qty) as qty";
+                    $where_cart_list = "id_barang = '" . $kode . "' AND id_sumber = '" . $id_toko . "' AND stat = 0 GROUP BY sn, sds, user_id";
+                    $cart_list_cache = $this->db(0)->get_cols_where('master_mutasi', $cols_cart, $where_cart_list);
+                }
+                $masuk[$key]['cart_list'] = $cart_list_cache;
             }
         }
 
@@ -31,110 +35,84 @@ class Barang extends Controller
 
     function stok_data_all($kode, $id_toko)
     {
-        $cols = "id_barang, sum(qty) as qty";
-        $where_masuk = "id_barang = '" . $kode . "' AND id_target = '" . $id_toko . "' AND stat = 1 GROUP BY id_barang";
-        $where_keluar = "id_barang = '" . $kode . "' AND id_sumber = '" . $id_toko . "' AND stat <> 2 GROUP BY id_barang";
-        $where_cart = "id_barang = '" . $kode . "' AND id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2 GROUP BY id_barang";
+        $cols = "id_barang, 
+            (SUM(CASE WHEN id_target = '" . $id_toko . "' AND stat = 1 THEN qty ELSE 0 END) - 
+            SUM(CASE WHEN id_sumber = '" . $id_toko . "' AND stat <> 2 THEN qty ELSE 0 END)) as qty,
+            SUM(CASE WHEN id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2 THEN qty ELSE 0 END) as cart";
 
-        $masuk = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_masuk, 1, "id_barang");
-        $keluar = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_keluar, 1, "id_barang");
-        $cart = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_cart, 1, "id_barang");
+        $where = "id_barang = '" . $kode . "' AND 
+            ((id_target = '" . $id_toko . "' AND stat = 1) OR 
+            (id_sumber = '" . $id_toko . "' AND stat <> 2) OR 
+            (id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2))
+            GROUP BY id_barang";
 
-        foreach ($masuk as $key => $ms) {
-            if (isset($keluar[$key])) {
-                $masuk[$key]['qty'] -= $keluar[$key]['qty'];
-            }
-            if (isset($cart[$key])) {
-                $masuk[$key]['cart'] = $cart[$key]['qty'];
-            }
-        }
+        $masuk = $this->db(0)->get_cols_where('master_mutasi', $cols, $where, 1, "id_barang");
 
         return $masuk;
     }
 
     function stok_data_list($id_toko)
     {
-        $cols = "id_barang, CONCAT(id_barang,'#',sn,sds) as unic, sn, sds, sum(qty) as qty";
-        $where_masuk = "id_target = '" . $id_toko . "' AND stat = 1 GROUP BY id_barang, sn, sds";
-        $where_keluar = "id_sumber = '" . $id_toko . "' AND stat <> 2 GROUP BY id_barang, sn, sds";
-        $where_cart = "id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2 GROUP BY id_barang, sn, sds";
+        $cols = "id_barang, CONCAT(id_barang,'#',sn,sds) as unic, sn, sds, 
+            (SUM(CASE WHEN id_target = '" . $id_toko . "' AND stat = 1 THEN qty ELSE 0 END) - 
+            SUM(CASE WHEN id_sumber = '" . $id_toko . "' AND stat <> 2 THEN qty ELSE 0 END)) as qty,
+            SUM(CASE WHEN id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2 THEN qty ELSE 0 END) as cart";
 
-        $masuk = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_masuk, 1, "unic");
-        $keluar = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_keluar, 1, "unic");
-        $cart = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_cart, 1, "unic");
+        $where = "((id_target = '" . $id_toko . "' AND stat = 1) OR 
+                  (id_sumber = '" . $id_toko . "' AND stat <> 2) OR 
+                  (id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2))
+                  GROUP BY id_barang, sn, sds";
 
-        foreach ($masuk as $key => $ms) {
-            if (isset($keluar[$key])) {
-                $masuk[$key]['qty'] -= $keluar[$key]['qty'];
-            }
-            if (isset($cart[$key])) {
-                $masuk[$key]['cart'] = $cart[$key]['qty'];
-            }
-        }
+        $masuk = $this->db(0)->get_cols_where('master_mutasi', $cols, $where, 1, "unic");
 
         return $masuk;
     }
 
     function stok_data_list_all($id_toko)
     {
-        $cols = "id_barang, sum(qty) as qty";
-        $where_masuk = "id_target = '" . $id_toko . "' AND stat = 1 GROUP BY id_barang";
-        $where_keluar = "id_sumber = '" . $id_toko . "' AND stat <> 2 GROUP BY id_barang";
-        $where_cart = "id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2 GROUP BY id_barang";
+        $cols = "id_barang, 
+            (SUM(CASE WHEN id_target = '" . $id_toko . "' AND stat = 1 THEN qty ELSE 0 END) - 
+            SUM(CASE WHEN id_sumber = '" . $id_toko . "' AND stat <> 2 THEN qty ELSE 0 END)) as qty,
+            SUM(CASE WHEN id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2 THEN qty ELSE 0 END) as cart";
 
-        $masuk = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_masuk, 1, "id_barang");
-        $keluar = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_keluar, 1, "id_barang");
-        $cart = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_cart, 1, "id_barang");
+        $where = "((id_target = '" . $id_toko . "' AND stat = 1) OR 
+                  (id_sumber = '" . $id_toko . "' AND stat <> 2) OR 
+                  (id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2))
+                  GROUP BY id_barang";
 
-        foreach ($masuk as $key => $ms) {
-            if (isset($keluar[$key])) {
-                $masuk[$key]['qty'] -= $keluar[$key]['qty'];
-            }
-            if (isset($cart[$key])) {
-                $masuk[$key]['cart'] = $cart[$key]['qty'];
-            }
-        }
+        $masuk = $this->db(0)->get_cols_where('master_mutasi', $cols, $where, 1, "id_barang");
 
         return $masuk;
     }
 
     function stok_data_web($id_toko = 1)
     {
-        $cols = "id_barang as id, sum(qty) as stock";
-        $where_masuk = "id_target = '" . $id_toko . "' AND stat = 1 GROUP BY id";
-        $where_keluar = "id_sumber = '" . $id_toko . "' AND stat <> 2 GROUP BY id";
+        $cols = "id_barang as id, 
+            (SUM(CASE WHEN id_target = '" . $id_toko . "' AND stat = 1 THEN qty ELSE 0 END) - 
+            SUM(CASE WHEN id_sumber = '" . $id_toko . "' AND stat <> 2 THEN qty ELSE 0 END)) as stock";
 
-        $masuk = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_masuk, 1, "id");
-        $keluar = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_keluar, 1, "id");
+        $where = "((id_target = '" . $id_toko . "' AND stat = 1) OR 
+                  (id_sumber = '" . $id_toko . "' AND stat <> 2))
+                  GROUP BY id_barang";
 
-        foreach ($masuk as $key => $ms) {
-            if (isset($keluar[$key])) {
-                $masuk[$key]['stock'] -= $keluar[$key]['stock'];
-            }
-        }
+        $masuk = $this->db(0)->get_cols_where('master_mutasi', $cols, $where, 1, "id");
 
         return $masuk;
     }
 
     function stok_data_list_sds($id_toko)
     {
-        $cols = "id_barang, CONCAT(id_barang,'#',sds) as unic, sds, sum(qty) as qty";
-        $where_masuk = "id_target = '" . $id_toko . "' AND stat = 1 GROUP BY id_barang, sds";
-        $where_keluar = "id_sumber = '" . $id_toko . "' AND stat <> 2 GROUP BY id_barang, sds";
-        $where_cart = "id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2 GROUP BY id_barang, sds";
+        $cols = "id_barang, CONCAT(id_barang,'#',sds) as unic, sds, 
+            (SUM(CASE WHEN id_target = '" . $id_toko . "' AND stat = 1 THEN qty ELSE 0 END) - 
+            SUM(CASE WHEN id_sumber = '" . $id_toko . "' AND stat <> 2 THEN qty ELSE 0 END)) as qty,
+            SUM(CASE WHEN id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2 THEN qty ELSE 0 END) as cart";
 
-        $masuk = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_masuk, 1, "unic");
-        $keluar = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_keluar, 1, "unic");
-        $cart = $this->db(0)->get_cols_where('master_mutasi', $cols, $where_cart, 1, "unic");
+        $where = "((id_target = '" . $id_toko . "' AND stat = 1) OR 
+                  (id_sumber = '" . $id_toko . "' AND stat <> 2) OR 
+                  (id_sumber = '" . $id_toko . "' AND stat = 0 AND jenis = 2))
+                  GROUP BY id_barang, sds";
 
-        foreach ($masuk as $key => $ms) {
-            if (isset($keluar[$key])) {
-                $masuk[$key]['qty'] -= $keluar[$key]['qty'];
-            }
-            if (isset($cart[$key])) {
-                $masuk[$key]['cart'] = $cart[$key]['qty'];
-            }
-        }
+        $masuk = $this->db(0)->get_cols_where('master_mutasi', $cols, $where, 1, "unic");
 
         return $masuk;
     }
